cb7e5d9d6a0db109d8a1ec34fdf067b5ac2ae042
Shai Angress <shai.angress@gmail.com>
2016-10-19 17:06:32 +0300

    *. add ng-repeat *. cleanup

--- index.js
+++ index.js
@@ -5,15 +5,13 @@ let ng122Map = {
     'ng-if': '*ngIf',
     'ng-class': '[ngClass]',
     'ng-click': '(click)',
-    '$ctrl.': ''
+    '$ctrl.': '',
+    'ng-repeat': '*ngFor'
 }
 let path = _.get(process, 'argv[2]', null);
 console.log('path', path);
 let fileContent = fs.readFileSync(path, {}).toString();
-// let newFileContent = _.replace(fileContent, /ng-if/g, '*ngIf');
-// console.log('fileContent', newFileContent);
 
-// let newFileContent = fileContent;
 _.forIn(ng122Map, (ng2prop, ng1Prop) => {
     let regex = new RegExp(ng1Prop, 'g');
     fileContent = _.replace(fileContent, regex, ng2prop);
@@ -26,4 +24,3 @@ fs.writeFile(path, fileContent, err => {
     console.log('file is now in ng2 style!');
 });
 
-// console.log('fileContent', fileContent);
